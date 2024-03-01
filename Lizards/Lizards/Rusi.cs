2600616843b45b43c59d77e3e8435286c1246c3f
braingalala <161814394+braingalala@users.noreply.github.com>
2024-03-01 13:06:20 +0300

    Add files via upload

---
+++ Lizards/Lizards/Rusi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lizards
+{
+
+    class Rusi
+    {
+
+        public static int hp = 100;
+        public static int svs = 100;
+        public static int st = 100;
+        public static int money = 0;
+
+    private Random random = new Random();
+        private int baseDamage = 10;
+        private double critChance = 0.2;
+
+        public void Attack()
+        {
+            int damage = CalculateDamage();
+            Console.WriteLine($"Получен урон: {damage}");
+        }
+        private int CalculateDamage()
+        {
+            int damage = baseDamage;
+            if (random.NextDouble() < this.critChance)
+            {
+                damage *= 2;
+                Console.WriteLine("Критический урон! Урон удвоен!");
+            }
+            return damage;
+        }
+    }
+}
+
